3d388b602bab38d8eadee1433df7fffb19702853
Sushi Shreechand <sushisushanshi13@gmail.com>
2025-05-05 08:15:11 +0530

    batman

---
+++ src/main/java/com/EduSync/EduSyncApplication.java
@@ -0,0 +1,13 @@
+package com.EduSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EduSync/EduSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EduSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
