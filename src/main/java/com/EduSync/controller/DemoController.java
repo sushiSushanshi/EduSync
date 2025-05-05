b5e94dd909ba5298b63508f14b49871308d37ebd
Sushi Shreechand <sushisushanshi13@gmail.com>
2025-05-05 20:15:38 +0530

    added keycloak security

---
+++ src/main/java/com/EduSync/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.EduSync.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/message")
+    public String getMessge(){
+        return "You are seeing a secured message.....";
+    }
+}
